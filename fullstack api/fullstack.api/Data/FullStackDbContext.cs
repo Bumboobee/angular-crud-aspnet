b7453baebf0ac3f40ee5163743c3c4b1b3f054a3
José Aparecido <juniorsilva240803@gmail.com>
2023-09-05 11:25:05 -0300

    initial commit

---
+++ fullstack api/fullstack.api/Data/FullStackDbContext.cs
@@ -0,0 +1,15 @@
+﻿using fullstack.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace fullstack.api.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
